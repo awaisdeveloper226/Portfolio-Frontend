2c998278e46859861123d46469b8295c8c8015fb
awaisdeveloper226 <muhammadawaisanas555@gmail.com>
2025-07-03 17:57:18 +0500

    sdjcvhbds

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -64,12 +64,7 @@ export default function blogs() {
                   frameworks. The best articles,links and news related to web
                   and mobile development
                 </p>
-                <div className="subemail">
-                  <form action="" className="flex">
-                    <input type="text" placeholder="Search blogs here" />
-                    <button>Search</button>
-                  </form>
-                </div>
+                
               </div>
             </div>
             <div className="featured">
@@ -135,65 +130,7 @@ export default function blogs() {
           </div>
         </section>
 
-        {/* Change the Category in url of each blog category   */}
-        <section className="populartegssec">
-          <div className="container">
-            <div className="border"></div>
-            <div className="populartegsdata">
-              <div className="fetitle">
-                <h3>Popular Tags</h3>
-              </div>
-              <div className="poputegs">
-                <Link href="/blog/category/Next JS" className="ptag">
-                  <img src="/img/nextjs.jpeg" alt="" />
-                  <div className="tegs">
-                    <div className="apps">
-                      <span></span>
-                      Next Js
-                    </div>
-                  </div>
-                </Link>
-                <Link href="/blog/category/Next JS" className="ptag">
-                  <img src="/img/nextjs.jpeg" alt="" />
-                  <div className="tegs">
-                    <div className="apps">
-                      <span></span>
-                      Next Js
-                    </div>
-                  </div>
-                </Link>
-                <Link href="/blog/category/Next JS" className="ptag">
-                  <img src="/img/nextjs.jpeg" alt="" />
-                  <div className="tegs">
-                    <div className="apps">
-                      <span></span>
-                      Next Js
-                    </div>
-                  </div>
-                </Link>
-                <Link href="/blog/category/Next JS" className="ptag">
-                  <img src="/img/nextjs.jpeg" alt="" />
-                  <div className="tegs">
-                    <div className="apps">
-                      <span></span>
-                      Next Js
-                    </div>
-                  </div>
-                </Link>
-                <Link href="/blog/category/Next JS" className="ptag">
-                  <img src="/img/nextjs.jpeg" alt="" />
-                  <div className="tegs">
-                    <div className="apps">
-                      <span></span>
-                      Next Js
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </section>
-
+    
         <section className="latestpostsec">
           <div className="container">
             <div className="border"></div>
@@ -209,24 +146,11 @@ export default function blogs() {
                     {publishedData.map((blog) => {
                       return (
                         <div className="lpost" key={blog._id}>
-                          <div className="lposting">
+                          <div className="lpostimg">
                             <Link href={`/blogs/${blog.slug}`}>
                               <img src={blog.images[0]} alt={blog.title} />
                             </Link>
-                            <div className="tegs">
-                              {blog.blogcategory.map((cat) => {
-                                return (
-                                  <Link
-                                    href={`/blogs/category/${cat}`}
-                                    className="ai"
-                                    key={cat}
-                                  >
-                                    <span></span>
-                                    {cat}
-                                  </Link>
-                                );
-                              })}
-                            </div>
+                            
                           </div>
                           <div className="lpostinfo">
                             <h3>
@@ -235,11 +159,9 @@ export default function blogs() {
                               </Link>
                             </h3>
                             <p>
-                              Lorem ipsum dolor sit amet consectetur adipisicing
-                              elit. Quibusdam itaque fuga ipsam adipisci
-                              quisquam aut, ad dignissimos, id in earum sed
-                              magnam voluptatem corrupti odio sequi. Possimus
-                              accusamus voluptas officiis.
+                              {blog.description.length > 100
+                                ? blog.description.slice(0, 100) + "..."
+                                : blog.description}
                             </p>
                             <h4 className="flex">
                               <img src="/img/me.png" alt="" />{" "}
@@ -254,40 +176,7 @@ export default function blogs() {
               </div>
             </div>
 
-            {publishedData.length === 0 ? (
-              ""
-            ) : (
-              <div className="blogspaginationbtn flex flex-center mt-3">
-                <button
-                  onClick={() => paginate(currentpage - 1)}
-                  disabled={currentpage === 1}
-                >
-                  Previous
-                </button>
-
-                {pagenumbers
-                  .slice(
-                    Math.max(currentpage - 3, 0),
-                    Math.min(currentpage + 2, pagenumbers.length)
-                  )
-                  .map((num) => (
-                    <button
-                      key={num}
-                      onClick={() => paginate(num)}
-                      className={`${currentpage === num ? "active" : ""}`}
-                    >
-                      {num}
-                    </button>
-                  ))}
-
-                <button
-                  onClick={() => paginate(currentpage + 1)}
-                  disabled={currentpage === pagenumbers.length}
-                >
-                  Next
-                </button>
-              </div>
-            )}
+          
           </div>
         </section>
       </div>
